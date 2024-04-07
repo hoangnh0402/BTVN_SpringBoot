4ca08d09f54b57429ccd63c4dc04ae63dff295b4
hoangnh4204 <hoangnh4204@gmail.com>
2024-04-07 09:24:49 +0700

    btvn tuan3

---
+++ BTVN_Week3/src/main/java/com/example/btvn_week3/DemoApplication.java
@@ -0,0 +1,10 @@
+package com.example.btvn_week3;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ BTVN_Week3/src/test/java/com/example/btvn_week3/BtvnWeek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btvn_week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtvnWeek3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
